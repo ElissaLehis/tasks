42053900dc4c01aa99435f07c5079b577757e896
Elissa Lehis <elissa.lehis@khk.ee>
2021-11-30 16:43:44 +0200

    Deletetask funktsiooni kasutamine 30.11.2021

--- app.js
+++ app.js
@@ -2,6 +2,31 @@
 const form = document.querySelector('form')
 form.addEventListener('submit', addTask)
 
+//task list
+const taskList = document.querySelector('ul')
+taskList.addEventListener('click', delTask)
+
+//delete button-link
+const deleteBtn = document.querySelector('#delete-tasks')
+deleteBtn.addEventListener('click', delTasks)
+
+function delTasks() {
+    //taskList.innerHTML = ''
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild)
+    }
+}
+
+//delTask
+function delTask(event){
+    if (event.target.textContent === 'X'){
+        if(confirm('Do you really want to delete this task? ')){
+            event.target.parentElement.remove()
+        }
+    }
+}
+
+
 // addTask function
 function addTask(event){
     // get task value from input
@@ -20,4 +45,19 @@ function addTask(event){
     // clear form input value
     document.querySelector('#task').value = ''
     event.preventDefault()
+
+    const link = document.createElement('a')
+    link.className =' secondary-content'
+    link.appendChild(document.createTextNode('X'))
+    link.setAttribute('href', 'a')
+    li.appendChild(link)
+
+    //get element from DO
+    taskList.appendChild(li)
+    //clear from input value
+    document.querySelector('#Task').value= ''
+    event.preventDefault()
+
+    console.log(taskList)
+
 }
