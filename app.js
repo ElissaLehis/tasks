f75b778012744cbf2a335c2acd0ebe4ee2ca620f
Elissa Lehis <elissa.lehis@khk.ee>
2021-12-01 10:25:21 +0200

    Added task deleting from localstorage function 1.12.2021

--- app.js
+++ app.js
@@ -22,10 +22,27 @@ function delTask(event){
     if (event.target.textContent === 'X'){
         if(confirm('Do you really want to delete this task? ')){
             event.target.parentElement.remove()
+            let task = event.target.parentElement.textContent.slice(0, -1)
+            removeStorage(task)
         }
     }
 }
 
+//removeStorage
+function removeStorage(task){
+    let tasks
+    if (localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach(function (taskFromLS, taskIndex){
+        if (taskFromLS === task){
+            tasks.splice(taskIndex, 1)
+        }
+    })
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
 
 // addTask function
 function addTask(event){
@@ -57,7 +74,7 @@ function addTask(event){
     //save task to localstorage
     taskStorage(task)
     //clear from input value
-    document.querySelector('#Task').value= ''
+    document.querySelector('#task').value = ''
     event.preventDefault()
 }
 
